4d68dad406638fc1ba9022b3a6878ec213e31339
Raul F. de Melo <melo.raulf@gmail.com>
2017-09-06 23:55:23 +0100

    Organized the code and implement input validation method

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
+/* Requires */
 const VMasker = require('vanilla-masker')
 const Template = require('./js/Template')
+// const Customer = require('./model/Customer')
+const CardFactory = require('./CardFactory')
 
 /* Elements */
 const $cpfInputElement = document.querySelector('.js-input-cpf')
 const $displayLineElement = document.querySelector('.display-line')
+const $errorSpan = document.querySelector('.input-error')
 
+/* Functions */
+/* eslint-disable */
+const cancelSubmit = event => event.preventDefault()
+/* eslint-enable */
+const isValidInput = () => {
+  const cpf = $cpfInputElement.value
+  const cpfRegex = /\d{3}.\d{3}.\d{3}-\d{2}/gi
+  return cpfRegex.test(cpf)
+}
+const handleErrorStatement = () => {
+  $errorSpan.classList.remove('is-hidden')
+  $cpfInputElement.classList.add('is-error')
+}
+
+/* Events and behaviour */
 VMasker($cpfInputElement).maskPattern('999.999.999-99')
-
-$displayLineElement.addEventListener('click', function (e) {
-  const isButton = e.target.classList.contains('button')
+$displayLineElement.addEventListener('click', function (event) {
+  const isButton = event.target.classList.contains('button')
 
   if (isButton) {
-    const isNewQuery = e.target.classList.contains('js-new-query')
+    const isNewQuery = event.target.classList.contains('js-new-query')
 
-    this.innerHTML = isNewQuery
-      ? Template.normalTemplate()
-      : Template.successTemplate({ name: 'Raul', total: 100 })
+    if (isNewQuery) {
+      this.innerHTML = Template.normalTemplate()
+      return
+    }
+    if (isValidInput()) {
+      this.innerHTML = Template.successTemplate({ name: 'Raul', total: 100 })
+    } else {
+      handleErrorStatement()
+    }
   }
 })
+
+CardFactory.queryPoints('').then(res => console.log(res))
