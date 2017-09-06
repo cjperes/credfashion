f7fa29c9f70ac448583e6379a1dcba22a277a603
Raul F. de Melo <melo.raulf@gmail.com>
2017-09-06 23:56:09 +0100

    Created the Customer class and made some tweaks into the template generator

--- src/js/Template.js
+++ src/js/Template.js
@@ -1,17 +1,19 @@
 class Template {
   static successTemplate(data) {
     return `
-        <div class="display-img"> <img src="img/credfashion.png"> </div>
-        <h2 class="welcome-msg">Olá, ${data.name}. Seu saldo é:</h2>
-        <h1 class="welcome-msg">R$ ${data.total}</h1>
-        <h2 class="welcome-msg">Aproveite para utilizar os seu pontos em qualquer uma de nossas lojas da Rede Etiqueta Modas!</h2>
+        <div class="display-img">
+          <img src="img/credfashion.png">
+        </div>
+        <p class="welcome-msg statement">Olá, ${data.name}. Seu saldo é:</p>
+        <p class="welcome-msg total-value">R$ ${data.total}</p>
+        <p class="welcome-msg hint">Aproveite para utilizar os seu pontos em qualquer uma de nossas lojas da Rede Etiqueta Modas!</p>
         <button class="js-new-query button">Nova Consulta</button>
     `
   }
 
   static normalTemplate() {
     return `
-    <form onsubmit="function(e){e.preventDefault()}">
+    <form onsubmit="cancelSubmit(e)">
         <div class="display-img"> <img src="img/credfashion.png"> </div>
         <h1 class="welcome-msg">Digite o seu CPF para realizar a consulta</h1>
         <input type="text" placeholder="" class="js-input-cpf" pattern="\\d{3}.\\d{3}.\\d{3}-\\d{2}">
